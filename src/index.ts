ae1b9d8f60b5f8754ce04754dd57e09238192b0b
Brian Joseph <brianjosephstudio@gmail.com>
2024-08-17 19:01:54 +0200

    first commit

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import axios from 'axios';
+
+export class Logger {
+  private identifier: string;
+  private baseUrl: string;
+
+  constructor(identifier: string) {
+    this.identifier = identifier;
+    this.baseUrl = 'https://cep-console.brianure.com';
+  }
+
+  async log(message: string): Promise<void> {
+    try {
+      await axios.post(`${this.baseUrl}/console/log`, {
+        identifier: this.identifier,
+        log: message
+      });
+    } catch (error) {
+      
+    }
+  }
+
+  async clear(): Promise<void> {
+    try {
+      await axios.post(`${this.baseUrl}/console/clear`, {
+        identifier: this.identifier
+      });
+    } catch (error) {
+      
+    }
+  }
+}
